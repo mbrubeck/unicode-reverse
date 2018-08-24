5c1bce0ebce540f29e4be495a9a72d91c1f60ca8
Matt Brubeck <mbrubeck@limpet.net>
2018-08-24 10:22:24 -0700

    Move tests to a separate file

--- src/lib.rs
+++ src/lib.rs
@@ -72,8 +72,10 @@
 #[cfg(test)]
 #[macro_use]
 extern crate quickcheck;
-
 extern crate unicode_segmentation;
+
+#[cfg(test)]
+mod tests;
 
 use core::str;
 use unicode_segmentation::UnicodeSegmentation;
@@ -123,54 +125,4 @@ pub fn reverse_grapheme_clusters_in_place(s: &mut str) {
 
     // Each UTF-8 sequence is now in the right order.
     debug_assert!(str::from_utf8(s.as_bytes()).is_ok());
-}
-
-#[cfg(test)]
-mod tests {
-    use super::reverse_grapheme_clusters_in_place;
-    use unicode_segmentation::UnicodeSegmentation;
-
-    extern crate std;
-    use self::std::string::String;
-    use self::std::string::ToString;
-
-    fn test_rev(a: &str, b: &str) {
-        let mut a = a.to_string();
-        reverse_grapheme_clusters_in_place(&mut a);
-        assert_eq!(a, b);
-    }
-
-    #[test]
-    fn test_empty() {
-        test_rev("", "");
-    }
-
-    #[test]
-    fn test_ascii() {
-        test_rev("Hello", "olleH");
-    }
-
-    #[test]
-    fn test_utf8() {
-        test_rev("¡Hola!", "!aloH¡");
-    }
-
-    #[test]
-    fn test_emoji() {
-        test_rev("\u{1F36D}\u{1F36E}", "\u{1F36E}\u{1F36D}");
-    }
-
-    #[test]
-    fn test_combining_mark() {
-        test_rev("man\u{0303}ana", "anan\u{0303}am");
-    }
-
-    quickcheck! {
-        fn quickcheck(s: String) -> bool {
-            let mut in_place = s.clone();
-            reverse_grapheme_clusters_in_place(&mut in_place);
-            let normal = s.graphemes(true).rev().collect::<String>();
-            in_place == normal
-        }
-    }
 }
